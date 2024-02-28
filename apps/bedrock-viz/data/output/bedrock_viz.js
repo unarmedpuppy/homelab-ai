85815d970e03fe858ebb95d3586428f465211868
Joshua Jenquist <github@jenquist.com>
2024-02-28 09:49:50 -0600

    syncing...

--- apps/bedrock-viz/data/output/bedrock_viz.js
+++ apps/bedrock-viz/data/output/bedrock_viz.js
@@ -2122,8 +2122,8 @@ function initDimension() {
             view: new ol.View({
                 projection: projection,
                 center: [
-                    dimensionInfo[globalDimensionId].playerPosX + Math.floor(dimensionInfo[globalDimensionId].worldWidth / 8)
-                   ,dimensionInfo[globalDimensionId].playerPosY + Math.floor(dimensionInfo[globalDimensionId].worldHeight / 12)
+                    dimensionInfo[globalDimensionId].playerPosX + Math.floor(dimensionInfo[globalDimensionId].worldWidth / 10)
+                   ,dimensionInfo[globalDimensionId].playerPosY + Math.floor(dimensionInfo[globalDimensionId].worldHeight / 20)
                 ],
                 resolution: 1
             })
