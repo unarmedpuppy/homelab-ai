095c8e0f10d8d3aa4935d49e5dd44c38fe5a6a0d
Joshua Jenquist <github@jenquist.com>
2024-03-06 22:21:20 -0600

    syncing...

--- scripts/backup-rust.sh
+++ scripts/backup-rust.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dest_dir="backup-$(date +%Y%m%d)"
 mkdir -p "$dest_dir"
-#cp -pf $(find ~/server/apps/rust/data/rust/server/unarmedpuppy -type f -name '*player*') "$dest_dir"
+cp -pf $(find ~/server/apps/rust/data/rust/server/unarmedpuppy -type f -name '*player*') "$dest_dir"
