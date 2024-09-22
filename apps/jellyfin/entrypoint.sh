775b98cbe06e813a6da3d5cecc2ee846fbfda3bb
Joshua Jenquist <github@jenquist.com>
2024-09-22 15:58:40 -0500

    syncing...

--- apps/jellyfin/entrypoint.sh
+++ apps/jellyfin/entrypoint.sh
@@ -2,9 +2,18 @@
 
 echo "attempting to mount ZFS..."
 source .env
+
 # Load the ZFS key and mount the dataset
 echo "$ZFS_KEY" | zfs load-key -a
 zfs mount -a
+
+# Wait until the ZFS dataset is mounted
+until mountpoint -q "$ZFS_PATH"; do
+  echo "Waiting for ZFS dataset to be mounted..."
+  sleep 1
+done
+
+echo "ZFS dataset mounted. Starting the application..."
 
 # Execute the CMD
 exec "$@"
