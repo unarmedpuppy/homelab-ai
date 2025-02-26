7c4e289ea03ccf16f3c6448be64d374c4d108e71
Joshua Jenquist <github@jenquist.com>
2025-02-26 09:11:33 -0600

    syncing...

--- apps/tcgplayer-scraper/tcg.py
+++ apps/tcgplayer-scraper/tcg.py
@@ -172,6 +172,6 @@ if __name__ == "__main__":
     
     print("Starting Flask application...")
     try:
-        app.run(debug=True, use_reloader=False)
+        app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
     except Exception as e:
         print(f"Flask failed to start: {e}")
