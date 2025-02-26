7c660f5db785d79f35d8aaa07d44a3c4bece4012
Joshua Jenquist <github@jenquist.com>
2025-02-26 09:31:40 -0600

    syncing...

--- apps/tcgplayer-scraper/tcg.py
+++ apps/tcgplayer-scraper/tcg.py
@@ -8,6 +8,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
 import os
 
 app = Flask(__name__)
